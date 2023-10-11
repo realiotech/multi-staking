9751b45ea307e3cd0d29a186fea23657c7075a95
Hieu Vu <gundamaster5@gmail.com>
2023-10-11 19:47:20 +0700

    add store methods

--- x/multi-staking/keeper/store.go
+++ x/multi-staking/keeper/store.go
@@ -3,15 +3,16 @@ package keeper
 import (
 	"fmt"
 
+	"cosmossdk.io/math"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/realio-tech/multi-staking-module/x/multi-staking/types"
 )
 
-func (k Keeper) GetBondTokenWeight(ctx sdk.Context, tokenDenom string) sdk.Dec {
+func (k Keeper) GetBondTokenWeight(ctx sdk.Context, tokenDenom string) math.LegacyDec {
 	store := ctx.KVStore(k.storeKey)
 	bz := store.Get(types.GetBondTokenWeightKey(tokenDenom))
 
-	bondTokenWeight := &sdk.Dec{}
+	bondTokenWeight := &math.LegacyDec{}
 	err := bondTokenWeight.Unmarshal(bz)
 	if err != nil {
 		panic(fmt.Errorf("unable to unmarshal bond token weight %v", err))
@@ -19,4 +20,75 @@ func (k Keeper) GetBondTokenWeight(ctx sdk.Context, tokenDenom string) sdk.Dec {
 	}
 
 	return *bondTokenWeight
+}
+
+func (k Keeper) SetBondTokenWeight(ctx sdk.Context, tokenDenom string, tokenWeight math.LegacyDec) {
+	store := ctx.KVStore(k.storeKey)
+	bz, err := tokenWeight.Marshal()
+
+	if err != nil {
+		panic(fmt.Errorf("unable to marshal bond token weight %v", err))
+	}
+
+	store.Set(types.GetBondTokenWeightKey(tokenDenom), bz)
+}
+
+func (k Keeper) GetValidatorBondDenom(ctx sdk.Context, operatorAddr sdk.ValAddress) string {
+	store := ctx.KVStore(k.storeKey)
+	bz := store.Get(types.GetValidatorBondDenomKey(operatorAddr))
+
+	return string(bz)
+}
+
+func (k Keeper) SetValidatorBondDenom(ctx sdk.Context, operatorAddr sdk.ValAddress, bondDenom string) {
+	store := ctx.KVStore(k.storeKey)
+
+	store.Set(types.GetValidatorBondDenomKey(operatorAddr), []byte(bondDenom))
+}
+
+func (k Keeper) GetIntermediaryAccountDelegator(ctx sdk.Context, intermediaryAccount sdk.AccAddress) sdk.AccAddress {
+	store := ctx.KVStore(k.storeKey)
+	bz := store.Get(types.GetIntermediaryAccountDelegatorKey(intermediaryAccount))
+
+	return bz
+}
+
+func (k Keeper) SetIntermediaryAccountDelegator(ctx sdk.Context, intermediaryAccount sdk.AccAddress, delegator sdk.AccAddress) {
+	store := ctx.KVStore(k.storeKey)
+
+	store.Set(types.GetIntermediaryAccountDelegatorKey(intermediaryAccount), delegator)
+}
+
+func (k Keeper) GetDVPairSDKBondTokens(ctx sdk.Context, delAddr sdk.AccAddress, valAddr sdk.ValAddress) sdk.Coin {
+	store := ctx.KVStore(k.storeKey)
+
+	bz := store.Get(types.GetDVPairSDKBondTokensKey(delAddr, valAddr))
+	var sdkBondTokens sdk.Coin
+	k.cdc.MustUnmarshal(bz, &sdkBondTokens)
+
+	return sdkBondTokens
+}
+
+func (k Keeper) SetDVPairSDKBondTokens(ctx sdk.Context, delAddr sdk.AccAddress, valAddr sdk.ValAddress, sdkBondTokens sdk.Coin) {
+	store := ctx.KVStore(k.storeKey)
+
+	bz := k.cdc.MustMarshal(&sdkBondTokens)
+	store.Set(types.GetDVPairSDKBondTokensKey(delAddr, valAddr), bz)
+}
+
+func (k Keeper) GetDVPairBondTokens(ctx sdk.Context, delAddr sdk.AccAddress, valAddr sdk.ValAddress) sdk.Coin {
+	store := ctx.KVStore(k.storeKey)
+
+	bz := store.Get(types.GetDVPairBondTokensKey(delAddr, valAddr))
+	var bondTokens sdk.Coin
+	k.cdc.MustUnmarshal(bz, &bondTokens)
+
+	return bondTokens
+}
+
+func (k Keeper) SetDVPairBondTokens(ctx sdk.Context, delAddr sdk.AccAddress, valAddr sdk.ValAddress, bondTokens sdk.Coin) {
+	store := ctx.KVStore(k.storeKey)
+
+	bz := k.cdc.MustMarshal(&bondTokens)
+	store.Set(types.GetDVPairBondTokensKey(delAddr, valAddr), bz)
 }
