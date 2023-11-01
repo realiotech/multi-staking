48c3a67ae039167023f21687533aca41be7d48c0
Tien Dung <neitdung@gmail.com>
2023-11-01 19:02:58 +0700

    add success test

--- x/multi-staking/keeper/msg_server_test.go
+++ x/multi-staking/keeper/msg_server_test.go
@@ -1,6 +1,8 @@
 package keeper_test
 
 import (
+	"time"
+
 	"cosmossdk.io/math"
 	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -9,6 +11,7 @@ import (
 	"github.com/realio-tech/multi-staking-module/testutil"
 	multistakingkeeper "github.com/realio-tech/multi-staking-module/x/multi-staking/keeper"
 	multistakingtypes "github.com/realio-tech/multi-staking-module/x/multi-staking/types"
+	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
 )
 
 func (suite *KeeperTestSuite) TestCreateValidator() {
@@ -197,142 +200,65 @@ func (suite *KeeperTestSuite) TestEditValidator() {
 	valPubKey := testutil.GenPubKey()
 	valAddr := sdk.ValAddress(valPubKey.Address())
 	gasDenom := "ario"
-	// govDenom := "arst"
+
 	testCases := []struct {
 		name     string
-		malleate func(ctx sdk.Context, msKeeper *multistakingkeeper.Keeper, msgServer multistakingtypes.MsgServer) (multistakingtypes.MsgEditValidator, error)
+		malleate func(ctx sdk.Context, msgServer multistakingtypes.MsgServer) (multistakingtypes.MsgEditValidator, error)
 		expErr   bool
 	}{
 		{
 			name: "success",
-			malleate: func(ctx sdk.Context, msKeeper *multistakingkeeper.Keeper, msgServer multistakingtypes.MsgServer) (multistakingtypes.MsgEditValidator, error) {
-				msKeeper.SetBondTokenWeight(ctx, gasDenom, math.LegacyMustNewDecFromStr("0.3"))
-				bondAmount := sdk.NewCoin(gasDenom, sdk.NewInt(3001))
-				createMsg := multistakingtypes.MsgCreateValidator{
-					Description: stakingtypes.Description{
-						Moniker:         "test",
-						Identity:        "test",
-						Website:         "test",
-						SecurityContact: "test",
-						Details:         "test",
-					},
-					Commission: stakingtypes.CommissionRates{
-						Rate:          sdk.MustNewDecFromStr("0.05"),
-						MaxRate:       sdk.MustNewDecFromStr("0.1"),
-						MaxChangeRate: sdk.MustNewDecFromStr("0.05"),
-					},
-					MinSelfDelegation: sdk.NewInt(1),
-					DelegatorAddress:  delAddr.String(),
-					ValidatorAddress:  valAddr.String(),
-					Pubkey:            codectypes.UnsafePackAny(valPubKey),
-					Value:             bondAmount,
-				}
+			malleate: func(ctx sdk.Context, msgServer multistakingtypes.MsgServer) (multistakingtypes.MsgEditValidator, error) {
 				newRate := sdk.MustNewDecFromStr("0.03")
 				newMinSelfDelegation := sdk.NewInt(100)
 				editMsg := multistakingtypes.NewMsgEditValidator(valAddr, stakingtypes.Description{
-						Moniker:         "test 1",
-						Identity:        "test 1",
-						Website:         "test 1",
-						SecurityContact: "test 1",
-						Details:         "test 1 ",
-					},
+					Moniker:         "test 1",
+					Identity:        "test 1",
+					Website:         "test 1",
+					SecurityContact: "test 1",
+					Details:         "test 1 ",
+				},
 					&newRate,
 					&newMinSelfDelegation,
 				)
-				_, err := msgServer.CreateValidator(ctx, &createMsg)
-				_, err = msgServer.EditValidator(ctx, editMsg)
+				_, err := msgServer.EditValidator(ctx, editMsg)
 				return *editMsg, err
 			},
 			expErr: false,
 		},
-		// {
-		// 	name: "invalid bond token",
-		// 	malleate: func(ctx sdk.Context, msKeeper *multistakingkeeper.Keeper, msgServer multistakingtypes.MsgServer) (multistakingtypes.MsgEditValidator, error) {
-		// 		bondAmount := sdk.NewCoin(gasDenom, sdk.NewInt(25))
-		// 		msg := multistakingtypes.MsgCreateValidator{
-		// 			Description: stakingtypes.Description{
-		// 				Moniker:         "test",
-		// 				Identity:        "test",
-		// 				Website:         "test",
-		// 				SecurityContact: "test",
-		// 				Details:         "test",
-		// 			},
-		// 			Commission: stakingtypes.CommissionRates{
-		// 				Rate:          sdk.MustNewDecFromStr("0.05"),
-		// 				MaxRate:       sdk.MustNewDecFromStr("0.1"),
-		// 				MaxChangeRate: sdk.MustNewDecFromStr("0.05"),
-		// 			},
-		// 			MinSelfDelegation: sdk.NewInt(1),
-		// 			DelegatorAddress:  delAddr.String(),
-		// 			ValidatorAddress:  valAddr.String(),
-		// 			Pubkey:            codectypes.UnsafePackAny(valPubKey),
-		// 			Value:             bondAmount,
-		// 		}
-		// 		_, err := msgServer.CreateValidator(ctx, &msg)
-		// 		return bondAmount, err
-		// 	},
-		// 	expErr: true,
-		// },
-		// {
-		// 	name: "invalid validator address",
-		// 	malleate: func(ctx sdk.Context, msKeeper *multistakingkeeper.Keeper, msgServer multistakingtypes.MsgServer) (multistakingtypes.MsgEditValidator, error) {
-		// 		msKeeper.SetBondTokenWeight(ctx, gasDenom, math.LegacyMustNewDecFromStr("0.3"))
-		// 		bondAmount := sdk.NewCoin(gasDenom, sdk.NewInt(3001))
-
-		// 		msg := multistakingtypes.MsgCreateValidator{
-		// 			Description: stakingtypes.Description{
-		// 				Moniker: "NewValidator",
-		// 			},
-		// 			Commission: stakingtypes.CommissionRates{
-		// 				Rate:          sdk.MustNewDecFromStr("0.05"),
-		// 				MaxRate:       sdk.MustNewDecFromStr("0.1"),
-		// 				MaxChangeRate: sdk.MustNewDecFromStr("0.05"),
-		// 			},
-		// 			MinSelfDelegation: sdk.NewInt(1),
-		// 			DelegatorAddress:  delAddr.String(),
-		// 			ValidatorAddress:  sdk.AccAddress([]byte("invalid")).String(),
-		// 			Pubkey:            codectypes.UnsafePackAny(valPubKey),
-		// 			Value:             bondAmount,
-		// 		}
-
-		// 		_, err := msgServer.CreateValidator(ctx, &msg)
-		// 		return bondAmount, err
-		// 	},
-		// 	expErr: true,
-		// },
-		// {
-		// 	name: "nil delegation amount",
-		// 	malleate: func(ctx sdk.Context, msKeeper *multistakingkeeper.Keeper, msgServer multistakingtypes.MsgServer) (multistakingtypes.MsgEditValidator, error) {
-		// 		msKeeper.SetBondTokenWeight(ctx, gasDenom, math.LegacyMustNewDecFromStr("0.3"))
-
-		// 		msg := multistakingtypes.MsgCreateValidator{
-		// 			Description: stakingtypes.Description{
-		// 				Moniker: "NewValidator",
-		// 			},
-		// 			Commission: stakingtypes.CommissionRates{
-		// 				Rate:          sdk.MustNewDecFromStr("0.05"),
-		// 				MaxRate:       sdk.MustNewDecFromStr("0.1"),
-		// 				MaxChangeRate: sdk.MustNewDecFromStr("0.05"),
-		// 			},
-		// 			MinSelfDelegation: sdk.NewInt(1),
-		// 			DelegatorAddress:  delAddr.String(),
-		// 			ValidatorAddress:  valAddr.String(),
-		// 			Pubkey:            codectypes.UnsafePackAny(valPubKey),
-		// 			Value:             sdk.Coin{},
-		// 		}
-
-		// 		_, err := msgServer.CreateValidator(ctx, &msg)
-		// 		return sdk.Coin{}, err
-		// 	},
-		// 	expErr: true,
-		// },
 	}
 
 	for _, tc := range testCases {
 		tc := tc
 		suite.Run(tc.name, func() {
 			suite.SetupTest()
-			originMsg, err := tc.malleate(suite.ctx, suite.msKeeper, multistakingkeeper.NewMsgServerImpl(*suite.msKeeper))
+			msgServer := multistakingkeeper.NewMsgServerImpl(*suite.msKeeper)
+			suite.msKeeper.SetBondTokenWeight(suite.ctx, gasDenom, math.LegacyMustNewDecFromStr("0.3"))
+			bondAmount := sdk.NewCoin(gasDenom, sdk.NewInt(3001))
+
+			createMsg := multistakingtypes.MsgCreateValidator{
+				Description: stakingtypes.Description{
+					Moniker:         "test",
+					Identity:        "test",
+					Website:         "test",
+					SecurityContact: "test",
+					Details:         "test",
+				},
+				Commission: stakingtypes.CommissionRates{
+					Rate:          sdk.MustNewDecFromStr("0.05"),
+					MaxRate:       sdk.MustNewDecFromStr("0.1"),
+					MaxChangeRate: sdk.MustNewDecFromStr("0.05"),
+				},
+				MinSelfDelegation: sdk.NewInt(1),
+				DelegatorAddress:  delAddr.String(),
+				ValidatorAddress:  valAddr.String(),
+				Pubkey:            codectypes.UnsafePackAny(valPubKey),
+				Value:             bondAmount,
+			}
+			msgServer.CreateValidator(suite.ctx, &createMsg)
+
+			suite.ctx = suite.ctx.WithBlockHeader(tmproto.Header{Time: time.Now()})
+			originMsg, err := tc.malleate(suite.ctx, msgServer)
 
 			if tc.expErr {
 				suite.Require().Error(err)
