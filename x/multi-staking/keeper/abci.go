cd172941e9bd146160c4b3a657d8faab8bab6c75
GnaD13 <cpt.meoz@gmail.com>
2023-12-01 02:11:04 +0700

    multi staking endblock

--- x/multi-staking/keeper/abci.go
+++ x/multi-staking/keeper/abci.go
@@ -10,5 +10,7 @@ func (k Keeper) BeginBlocker(ctx sdk.Context) {
 }
 
 func (k Keeper) EndBlocker(ctx sdk.Context, unbondedStakings types.UnbondedMultiStakings) {
-
+	for _, unbondedStaking := range unbondedStakings.UnbondedMultiStakings {
+		k.BurnBondTokenAndUnlockMultiStakingToken(ctx, sdk.AccAddress(unbondedStaking.DelAddr), sdk.ValAddress(unbondedStaking.ValAddr), unbondedStaking.Amount[0])
+	}
 }
