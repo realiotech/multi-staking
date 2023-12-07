db67bd16014ce48c45bb84a0624f4010545e3eb3
DongLieu <duongduydongtq@gmail.com>
2023-12-07 15:24:26 +0700

    proto for proposals

---
+++ x/multi-staking/keeper/proposal_handler.go
@@ -0,0 +1,17 @@
+package keeper
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+func AddBondDenomProposal(ctx sdk.Context, k *Keeper) error {
+	return nil
+}
+
+func ChangeBondTokenWeightProposals(ctx sdk.Context, k *Keeper) error {
+	return nil
+}
+
+func RemoveBondTokenProposal(ctx sdk.Context, k *Keeper) error {
+	return nil
+}
