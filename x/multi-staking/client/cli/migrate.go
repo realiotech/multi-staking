2544c9f7cc34d009518712aa2efd5d485cac8433
GnaD13 <cpt.meoz@gmail.com>
2023-12-12 13:33:27 +0700

    cmd

--- x/multi-staking/client/cli/migrate.go
+++ x/multi-staking/client/cli/migrate.go
@@ -1,19 +1,83 @@
 package cli
 
 import (
+	"encoding/json"
+	"fmt"
+	"time"
+
+	"cosmossdk.io/errors"
+	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/client/flags"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/spf13/cobra"
+	tmjson "github.com/tendermint/tendermint/libs/json"
+	tmtypes "github.com/tendermint/tendermint/types"
 )
+
+type AppMap map[string]json.RawMessage
 
 const flagGenesisTime = "genesis-time"
 
 // MigrateGenesisCmd returns a command to execute genesis state migration.
 func MigrateStakingGenesisCmd() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "migrate-staking [target-version] [genesis-file]",
+		Use:   "migrate-staking [genesis-file]",
 		Short: "Migrate genesis to a specified target version",
-		Args:  cobra.ExactArgs(2),
+		Args:  cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx := client.GetClientContextFromCmd(cmd)
+
+			var err error
+			// Read genesis state
+			genesisPath := args[0]
+			genDoc, err := validateGenDoc(genesisPath)
+			if err != nil {
+				return err
+			}
+
+			var initialState AppMap
+			if err := json.Unmarshal(genDoc.AppState, &initialState); err != nil {
+				return errors.Wrap(err, "failed to JSON unmarshal initial genesis state")
+			}
+
+			// migrate
+			newGenState := migrate(initialState, clientCtx)
+			genDoc.AppState, err = json.Marshal(newGenState)
+			if err != nil {
+				return errors.Wrap(err, "failed to JSON marshal migrated genesis state")
+			}
+
+			// add genesis time
+			genesisTime, _ := cmd.Flags().GetString(flagGenesisTime)
+			if genesisTime != "" {
+				var t time.Time
+
+				err := t.UnmarshalText([]byte(genesisTime))
+				if err != nil {
+					return errors.Wrap(err, "failed to unmarshal genesis time")
+				}
+
+				genDoc.GenesisTime = t
+			}
+
+			// add chain-id
+			chainID, _ := cmd.Flags().GetString(flags.FlagChainID)
+			if chainID != "" {
+				genDoc.ChainID = chainID
+			}
+
+			bz, err := tmjson.Marshal(genDoc)
+			if err != nil {
+				return errors.Wrap(err, "failed to marshal genesis doc")
+			}
+
+			sortedBz, err := sdk.SortJSON(bz)
+			if err != nil {
+				return errors.Wrap(err, "failed to sort JSON genesis doc")
+			}
+
+			cmd.Println(string(sortedBz))
+
 			return nil
 		},
 	}
@@ -22,4 +86,23 @@ func MigrateStakingGenesisCmd() *cobra.Command {
 	cmd.Flags().String(flags.FlagChainID, "", "override chain_id with this flag")
 
 	return cmd
+}
+
+// validateGenDoc reads a genesis file and validates that it is a correct
+// Tendermint GenesisDoc. This function does not do any cosmos-related
+// validation.
+func validateGenDoc(importGenesisFile string) (*tmtypes.GenesisDoc, error) {
+	genDoc, err := tmtypes.GenesisDocFromFile(importGenesisFile)
+	if err != nil {
+		return nil, fmt.Errorf("%s. Make sure that"+
+			" you have correctly migrated all Tendermint consensus params",
+			err.Error(),
+		)
+	}
+
+	return genDoc, nil
+}
+
+func migrate(genesisState AppMap, ctx client.Context) AppMap {
+	return nil
 }
