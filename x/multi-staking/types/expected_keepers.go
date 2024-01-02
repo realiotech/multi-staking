c089b8575dbf2f92c46c1ad2e365e8318133d8fd
GnaD13 <cpt.meoz@gmail.com>
2024-01-02 17:43:10 +0700

    expected staking keeper genesis

--- x/multi-staking/types/expected_keepers.go
+++ x/multi-staking/types/expected_keepers.go
@@ -5,9 +5,12 @@ import (
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
+	abci "github.com/tendermint/tendermint/abci/types"
 )
 
 type StakingKeeper interface {
 	DequeueAllMatureUBDQueue(ctx sdk.Context, currTime time.Time) (matureUnbonds []stakingtypes.DVPair)
 	BondDenom(ctx sdk.Context) (res string)
+	InitGenesis(ctx sdk.Context, data *stakingtypes.GenesisState) (res []abci.ValidatorUpdate)
+	ExportGenesis(ctx sdk.Context) *stakingtypes.GenesisState
 }
