258a57ea4e7b7e1fba4824de871a6c56b6699130
Dong Lieu <duongduydongtq@gmail.com>
2024-01-13 22:20:11 +0700

    fix merge main

--- x/multi-staking/keeper/keeper_test.go
+++ x/multi-staking/keeper/keeper_test.go
@@ -3,27 +3,28 @@ package keeper_test
 import (
 	"testing"
 
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	stakingkeeper "github.com/cosmos/cosmos-sdk/x/staking/keeper"
 	"github.com/realio-tech/multi-staking-module/testing/simapp"
 	multistakingkeeper "github.com/realio-tech/multi-staking-module/x/multi-staking/keeper"
 	"github.com/stretchr/testify/suite"
 	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
-
-	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
 type KeeperTestSuite struct {
 	suite.Suite
 
-	ctx      sdk.Context
-	app      simapp.SimApp
-	msKeeper *multistakingkeeper.Keeper
+	ctx           sdk.Context
+	app           simapp.SimApp
+	msKeeper      *multistakingkeeper.Keeper
+	stakingKeeper *stakingkeeper.Keeper
 }
 
 func (suite *KeeperTestSuite) SetupTest() {
 	app := simapp.Setup(false)
 	ctx := app.BaseApp.NewContext(false, tmproto.Header{})
 
-	suite.ctx, suite.msKeeper = ctx, &app.MultiStakingKeeper
+	suite.app, suite.ctx, suite.msKeeper, suite.stakingKeeper = *app, ctx, &app.MultiStakingKeeper, &app.StakingKeeper
 }
 
 func TestKeeperTestSuite(t *testing.T) {
