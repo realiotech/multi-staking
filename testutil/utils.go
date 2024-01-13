3031ac157b0796aedb7bb4e805076f56ed737f84
Hieu Vu <gundamaster5@gmail.com>
2024-01-13 19:05:31 +0700

    remove redundant file

--- testutil/utils.go
+++
@@ -1,9 +0,0 @@
-package testutil
-
-const (
-	// MultistakingDenomA defines the multi staking denomination A
-	MultistakingDenomA = "ario"
-
-	// MultistakingDenomB defines the multi staking denomination B
-	MultistakingDenomB = "arst"
-)
