64ff1d6c493c9340bf77247a499b6022f9c761e5
Dong Lieu <duongduydongtq@gmail.com>
2024-01-13 22:21:28 +0700

    lint

--- x/multi-staking/keeper/keeper_test.go
+++ x/multi-staking/keeper/keeper_test.go
@@ -3,12 +3,13 @@ package keeper_test
 import (
 	"testing"
 
-	sdk "github.com/cosmos/cosmos-sdk/types"
-	stakingkeeper "github.com/cosmos/cosmos-sdk/x/staking/keeper"
 	"github.com/realio-tech/multi-staking-module/testing/simapp"
 	multistakingkeeper "github.com/realio-tech/multi-staking-module/x/multi-staking/keeper"
 	"github.com/stretchr/testify/suite"
 	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	stakingkeeper "github.com/cosmos/cosmos-sdk/x/staking/keeper"
 )
 
 type KeeperTestSuite struct {
