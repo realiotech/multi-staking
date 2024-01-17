3f8115018181ade9988efaba09667e17cb4cecee
GnaD13 <cpt.meoz@gmail.com>
2024-01-17 11:05:29 +0700

    nit

--- x/multi-staking/keeper/invariants.go
+++ x/multi-staking/keeper/invariants.go
@@ -73,7 +73,7 @@ func ValidatorLockDenomInvariants(k Keeper) sdk.Invariant {
 			if valMsDenom := k.GetValidatorMultiStakingCoin(ctx, sdk.ValAddress(valAddr)); valMsDenom != lock.LockedCoin.Denom {
 				broken = true
 				msg += fmt.Sprintf("validator lock denom invariants:\n\t lock denom: %v"+
-					"\n\tvalidator allow denom: %v\n",
+					"\n\tvalidator valMsDenom: %v\n",
 					lock.LockedCoin.Denom, valMsDenom)
 			}
 		}
