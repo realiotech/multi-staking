eb9ba22418ea8dd0cd2f8d1f21a88d8b483d67df
GnaD13 <cpt.meoz@gmail.com>
2024-01-19 18:04:08 +0700

    add setup test for invariants

---
+++ x/multi-staking/keeper/invartiants_test.go
@@ -0,0 +1,29 @@
+package keeper_test
+
+import (
+	"github.com/realio-tech/multi-staking-module/x/multi-staking/keeper"
+)
+
+func (suite *KeeperTestSuite) TestModuleAccountInvariants() {
+	testCases := []struct {
+		name     string
+		malleate func()
+		expPass  bool
+	}{
+		{
+			name:     "Success",
+			malleate: func() {},
+			expPass:  true,
+		},
+	}
+	for _, tc := range testCases {
+		suite.SetupTest() // reset
+		tc.malleate()
+		out, broken := keeper.ModuleAccountInvariants(*suite.msKeeper)(suite.ctx)
+
+		if tc.expPass {
+			suite.Require().False(broken)
+			suite.Require().Empty(out)
+		}
+	}
+}
