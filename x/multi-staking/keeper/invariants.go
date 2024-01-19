45f6ffd7412d9e44e01e72946d3f0cfc364b1308
GnaD13 <cpt.meoz@gmail.com>
2024-01-19 18:10:35 +0700

    nit

--- x/multi-staking/keeper/invariants.go
+++ x/multi-staking/keeper/invariants.go
@@ -42,7 +42,7 @@ func ModuleAccountInvariants(k Keeper) sdk.Invariant {
 		moduleAccount := authtypes.NewModuleAddress(types.ModuleName)
 		escrowBalances := k.bankKeeper.GetAllBalances(ctx, moduleAccount)
 
-		broken := !escrowBalances.IsEqual(totalLockCoinAmount)
+		broken := !escrowBalances.IsEqual(lockCoinAmount)
 
 		return sdk.FormatInvariant(
 			types.ModuleName,
