12d9ca588b228e4575e6d6901dcdf5a040bc7fd1
dzungdo <neitdung@gmail.com>
2024-01-29 14:52:20 +0700

    Add unpack interface for any type

--- x/multi-staking/types/genesis.go
+++ x/multi-staking/types/genesis.go
@@ -1,6 +1,7 @@
 package types
 
 import (
+	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
 	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
 )
 
@@ -15,5 +16,15 @@ func DefaultGenesis() *GenesisState {
 // Validate performs basic genesis state validation, returning an error upon any failure.
 // TODO: Add validate genesis
 func (gs GenesisState) Validate() error {
+	return nil
+}
+
+// UnpackInterfaces implements UnpackInterfacesMessage.UnpackInterfaces
+func (g GenesisState) UnpackInterfaces(c codectypes.AnyUnpacker) error {
+	for i := range g.StakingGenesisState.Validators {
+		if err := g.StakingGenesisState.Validators[i].UnpackInterfaces(c); err != nil {
+			return err
+		}
+	}
 	return nil
 }
