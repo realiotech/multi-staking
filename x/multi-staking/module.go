3b9e4b22a083bc48eae8b7ea84ad1b5e44187412
GnaD13 <cpt.meoz@gmail.com>
2024-02-22 15:07:53 +0700

    multistaking cli

--- x/multi-staking/module.go
+++ x/multi-staking/module.go
@@ -17,7 +17,6 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
 	"github.com/cosmos/cosmos-sdk/x/staking"
-	stakingcli "github.com/cosmos/cosmos-sdk/x/staking/client/cli"
 	stakingkeeper "github.com/cosmos/cosmos-sdk/x/staking/keeper"
 	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
 )
@@ -68,7 +67,7 @@ func (AppModuleBasic) RegisterGRPCGatewayRoutes(clientCtx client.Context, mux *g
 
 // GetTxCmd returns the staking module's root tx command.
 func (AppModuleBasic) GetTxCmd() *cobra.Command {
-	return stakingcli.NewTxCmd()
+	return cli.NewTxCmd()
 }
 
 // GetQueryCmd returns the multi-staking and staking module's root query command.
